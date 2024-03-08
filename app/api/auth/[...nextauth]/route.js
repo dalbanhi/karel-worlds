9b65a6d3740db798f364db4bd050224e0ad52311
dalbanhi <dalbanhi@gmail.com>
2024-03-07 19:24:23 -0600

    added routing and user providers. need to fix oauth client callback error

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,55 @@
+import NextAuth from "next-auth/next";
+
+import GoogleProvider from "next-auth/providers/google";
+import { connectToDatabase } from "@utils/database";
+
+import User from "@models/User";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbacakUrl: process.env.GOOGLE_CALLBACK_URL,
+        }),
+    ],
+    async session({session}){
+
+        const sessionUser = await User.findOne({
+            email: session.user.email,
+        });
+
+        session.user.id = sessionUser._id.toString();
+        return session;
+
+    },
+    async signIn({profile}){
+        try{
+            await connectToDatabase();
+            //checking if a user already exists
+            const userExistsAlready = await User.findOne({
+                email: profile.email
+            });
+
+            //if the user does not exist, make one
+            if(!userExistsAlready){
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", ".").toLowerCase(),
+                    image: profile.picture,
+
+                });
+            }
+
+            return true;
+        } catch(e){
+            console.log(e);
+            return false;
+
+        }
+
+    }
+
+})
+
+export {handler as GET, handler as POST}
