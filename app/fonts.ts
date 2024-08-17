163c92b0e46f367a7b1d4029e3c9737ee15cdfa1
dalbanhi <12940551+dalbanhi@users.noreply.github.com>
2024-08-17 18:12:44 -0500

    Added shadcn, theme and fonts

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Open_Sans, Montserrat } from "next/font/google";
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+});
