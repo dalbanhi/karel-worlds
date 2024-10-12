da016e536f3bbd8b3c00098c00cc85f4b35a4aab
dalbanhi <12940551+dalbanhi@users.noreply.github.com>
2024-10-12 15:33:41 -0500

    Added puzle editing and rating coming soon toast

--- app/(nosidebars)/puzzle/[id]/page.tsx
+++ app/(nosidebars)/puzzle/[id]/page.tsx
@@ -3,17 +3,25 @@ import Puzzle from "@/components/shared/puzzle/Puzzle";
 import { getPuzzle } from "@/lib/actions/puzzles";
 import { worldInfoType } from "@/types/karelWorld";
 import { getCurrentUser } from "@/lib/auth/checkUser";
+import PuzzleActionButtons from "@/components/shared/puzzle/PuzzleActionButtons";
 
 const PuzzlePage = async ({ params }: { params: { id: string } }) => {
   //get the id from the route params
 
   const puzzle = await getPuzzle(params.id);
   const currUser = await getCurrentUser();
+
   if (!puzzle) {
     return <div>loading...</div>;
   }
+  const hasSolved = currUser?.puzzlesSolved.includes(puzzle);
   return (
     <div className="w-full">
+      <PuzzleActionButtons
+        currentUserID={currUser?.id || ""}
+        puzzleID={puzzle.id}
+        hasSolved={hasSolved}
+      />
       <Puzzle
         worldDimensions={{
           width: puzzle.worldWidth,
---
+++ components/shared/puzzle/PuzzleActionButtons.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+
+interface PuzzleActionButtonsProps {
+  isCreator: boolean;
+  hasSolved: boolean;
+}
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Pencil1Icon, StarFilledIcon } from "@radix-ui/react-icons";
+import { toast } from "@/hooks/use-toast";
+
+const PuzzleActionButtons: React.FC<PuzzleActionButtonsProps> = ({
+  isCreator,
+  hasSolved,
+}) => {
+  return (
+    <div className="flex w-full justify-between">
+      {isCreator && (
+        <Button
+          onClick={() => {
+            toast({
+              variant: "warning",
+              title: "Coming soon",
+              description: "Puzzle editing is coming soon!",
+            });
+          }}
+          type="button"
+          variant="default"
+          className="flex gap-1 items-center"
+        >
+          {" "}
+          Edit <Pencil1Icon />
+        </Button>
+      )}
+      {hasSolved && (
+        <Button
+          onClick={() => {
+            toast({
+              variant: "warning",
+              title: "Coming soon",
+              description: "Puzzle re-rating is coming soon!",
+            });
+          }}
+          type="button"
+          variant="default"
+          className="flex gap-1 items-center"
+        >
+          {" "}
+          Rate <StarFilledIcon />
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default PuzzleActionButtons;
